9a53369dc6e3e38db0b759597171f37f31b4b9dc
stevenwyee <31084278+stevenwyee@users.noreply.github.com>
2018-10-20 15:19:49 -0700

    Ima aweosmoemewadwa
    Killzone complete. Respawns player to chosen spawnpoint

---
+++ Assets/Scripts/DeathOnCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathOnCollision : MonoBehaviour {
+    [SerializeField] Transform spawnPoint;
+
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Triggered!");
+        if (col.transform.CompareTag("Player"))
+            col.transform.position = spawnPoint.position;
+    }
+    
+
+    /*
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Fuck you");
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }  
+    */
+    
+}
