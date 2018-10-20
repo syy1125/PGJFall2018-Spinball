923154db9303948124177759a88d3abf61acbcd4
Eric Kim <ericpaulkim@gmail.com>
2018-10-19 18:29:17 -0700

    Test Scene + Test Bumper

---
+++ Assets/Bumper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBounceable { }
+
+public class Bumper : MonoBehaviour 
+{
+	public float bounceForce;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		Rigidbody2D rb = col.gameObject.GetComponent<Rigidbody2D>();
+		if(rb != null)
+		{
+			rb.AddForce( (this.transform.position - rb.gameObject.transform.position) * bounceForce );
+		}
+	}
+}
