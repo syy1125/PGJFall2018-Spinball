f903e9090905078513052f2c123b2c6dafdc97c1
syy1125 <syy1125@hotmail.com>
2018-10-20 15:27:07 -0700

    Implement spiral text generator
    Implement script that takes some content and turns it into spiral text.

---
+++ Assets/Scripts/TextSpiralController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.UI;
+
+public class TextSpiralController : MonoBehaviour
+{
+	private const float RAD = 180 / Mathf.PI;
+
+	[TextArea] public string Content;
+	public GameObject CharacterPrefab;
+
+	public float InitialRadius = 100;
+	public float TextScale = 0.2f;
+	public float ScaleMultiplier = 1;
+	public float AngleDelta;
+
+	private void Start()
+	{
+		float scale = 1;
+		float angle = Mathf.PI / 2;
+
+		for (int charIndex = 0; charIndex < Content.Length; charIndex++)
+		{
+			GameObject characterObject = Instantiate(
+				CharacterPrefab,
+				Vector3.zero,
+				Quaternion.AngleAxis(RAD * (angle - Mathf.PI / 2), Vector3.forward),
+				transform
+			);
+
+			characterObject.transform.localPosition = new Vector3(
+				InitialRadius * scale * Mathf.Cos(angle),
+				InitialRadius * scale * Mathf.Sin(angle),
+				0
+			);
+			characterObject.transform.localScale = new Vector3(scale * TextScale, scale * TextScale, scale * TextScale);
+			characterObject.GetComponent<Text>().text = Content[charIndex].ToString();
+
+			scale *= ScaleMultiplier;
+			angle -= AngleDelta;
+		}
+	}
+}
