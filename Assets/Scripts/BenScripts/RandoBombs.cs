b89aa5437021c5f96e658ac1b011b20bd45da435
KupoGrounds <btatnguyen@gmail.com>
2018-10-20 14:42:19 -0700

    random bomb generator ready

---
+++ Assets/Scripts/BenScripts/RandoBombs.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandoBombs : MonoBehaviour {
+
+    public GameObject bombPrefab;
+
+    public int countdown = 0;       //time before bomb dropping begin
+    public float cooldown = 0.0f;   //time between bomb drops
+
+    public float radiusLimit = 0.0f;
+
+    private float stopwatch;
+    private float bombStopwatch;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        stopwatch = 0.0f;
+        bombStopwatch = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        stopwatch += Time.deltaTime;
+        bombStopwatch += Time.deltaTime;
+
+        if (stopwatch >= countdown)
+        {
+            if (bombStopwatch > cooldown)
+            {
+                bombStopwatch = 0;
+                Vector2 placement = new Vector2(Random.insideUnitCircle.x * radiusLimit, Random.insideUnitCircle.y * radiusLimit);
+                GameObject bomb = (GameObject)Instantiate(bombPrefab, placement, Quaternion.identity);
+            }
+        }
+	}
+}
