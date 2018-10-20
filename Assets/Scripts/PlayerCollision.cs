afdf79d535c95b0049a17936dd9fe127bd1c21c3
Eric Kim <ericpaulkim@gmail.com>
2018-10-19 22:23:10 -0700

    Player Collision Working

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour 
+{
+	private Rigidbody2D rigidbody;
+
+	void Start () 
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		GameObject other = col.gameObject;
+		Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+		if(rb != null && other.CompareTag("Player"))
+		{
+			Debug.Log("mine : " + rigidbody.velocity);
+			Debug.Log("other: " + rb.velocity);
+			float mag = rigidbody.velocity.magnitude;
+			rigidbody.velocity = (transform.position - other.transform.position) * rb.velocity.magnitude;
+			rb.velocity = (other.transform.position - transform.position) * mag;
+		}
+	}
+}
