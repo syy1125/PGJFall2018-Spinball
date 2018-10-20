3505b91c6ae1d7bc7429bcc9b54ebe94044b8ed5
syy1125 <syy1125@hotmail.com>
2018-10-19 21:51:38 -0700

    Implement basic player death
    Player now has a way to die. Currently you press space to kill all players.
    Player death script also updates death counters on HUD.

---
+++ Assets/Scripts/DeathManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathManager : MonoBehaviour
+{
+	public float RespawnDelay;
+	public Text DeathDisplay;
+	private int _deaths;
+
+	private void Start()
+	{
+		_deaths = 0;
+		UpdateDeathDisplay();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Kill();
+		}
+	}
+
+	private void UpdateDeathDisplay()
+	{
+		DeathDisplay.text = _deaths.ToString();
+	}
+
+	private IEnumerator RespawnSequence()
+	{
+		gameObject.SetActive(false);
+		_deaths++;
+		UpdateDeathDisplay();
+
+		yield return new WaitForSeconds(RespawnDelay);
+
+		transform.position = new Vector3(1, 1, 0);
+		gameObject.SetActive(true);
+	}
+
+	public void Kill()
+	{
+		// Start coroutine on an active object - since RespawnSequence deactivates this object the rest of the coroutine would've halted.
+		GameStateManager.Instance.StartCoroutine(RespawnSequence());
+	}
+}
---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+	public static GameStateManager Instance;
+
+	private void Awake()
+	{
+		if (Instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			Instance = this;
+		}
+	}
+}
