d2212f8e64b72a524bc25be5b41446535324fe5f
KupoGrounds <btatnguyen@gmail.com>
2018-10-20 16:05:26 -0700

    Explosion fixed

--- Assets/Scripts/BenScripts/Explosive.cs
+++ Assets/Scripts/BenScripts/Explosive.cs
@@ -6,6 +6,42 @@ public class Explosive : MonoBehaviour {
 
     public int health = 3;
     public float blastForce = 0.0f;
+    public float bounceForce = 0.0f;
+    public float blastRadius = 1.0f;
+    public LayerMask mask = -1;
+
+    private void Explosion()
+    {
+        ContactFilter2D contact = new ContactFilter2D();
+        contact.layerMask = mask;
+        Collider2D[] results = new Collider2D[10];
+
+        int hitByBlast = Physics2D.OverlapCircle(this.transform.position, blastRadius, contact, results);
+        if (hitByBlast > 0)
+        {
+            for (int i = 0; i < hitByBlast; ++i)
+            {
+                Rigidbody2D rb = results[i].GetComponent<Rigidbody2D>();
+                if (rb != null)
+                {
+                    rb.velocity = Vector2.zero;
+                    rb.AddForce(rb.position - new Vector2(transform.position.x, transform.position.y) * blastForce);
+                }
+            }
+        }
+    }
+
+    private void Bounce(Collider2D collision)
+    {
+        Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+
+        if (rb != null)
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(rb.position - new Vector2(transform.position.x, transform.position.y) * bounceForce);
+        }
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,23 +49,15 @@ public class Explosive : MonoBehaviour {
         {
             --health;
 
-            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
-
-            if (rb != null)
-            {
-                if (collision.transform.position.x < this.transform.position.x) //applying force in the x direction, based on relative positions of barrel and collider
-                    rb.AddForce(new Vector2(-blastForce, 0));
-                else
-                    rb.AddForce(new Vector2(blastForce, 0));
-
-                if (collision.transform.position.y < this.transform.position.y) //applying force in the y direction, based on relative positions of barrel and collider
-                    rb.AddForce(new Vector2(0, -blastForce));
-                else
-                    rb.AddForce(new Vector2(0, blastForce));
-            }
-            
             if (health == 0)
+            {
+                Explosion();
                 Destroy(this.gameObject);
+            }
+            else
+            {
+                Bounce(collision);
+            }
         }
     }
 }
