508c31fb69741cc1dda3b0031a1ed96e1dfd596d
syy1125 <syy1125@hotmail.com>
2018-10-20 20:13:46 -0700

    Cache main camera
    Main camera is now cached because `Camera.main` is inefficient.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,19 +12,20 @@ public class CameraController : MonoBehaviour
 	public float minimumOrthographicSize;
 	public float maximumOrthographicSize;
 
-	//private
+	private Camera mainCamera;
 
 	void Start () 
 	{
 		this.transform.position = ((playerOne.transform.position + playerTwo.transform.position) / 2) + offset;
+		mainCamera = Camera.main;
 	}
 	
-	void LateUpdate () 
+	void LateUpdate ()
 	{
 		Vector3 midpoint = (playerOne.transform.position + playerTwo.transform.position) / 2;
 		float distance = Mathf.Min(Mathf.Max(minimumOrthographicSize, (playerOne.transform.position - playerTwo.transform.position).magnitude), maximumOrthographicSize);
 		this.transform.position = Vector3.Lerp(this.transform.position, midpoint + offset, cameraDamping);
-		Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, distance, cameraDamping);
+		mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, distance, cameraDamping);
 		//this.transform.position = toFollow.transform.position + offset;
 		//this.transform.position = Vector3.Lerp(this.transform.position, toFollow.transform.position + offset, cameraDamping);
 	}
