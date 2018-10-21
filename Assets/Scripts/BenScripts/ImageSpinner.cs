6747bf7803975a6c9953bdd9a8ce873a4a7cd7e4
KupoGrounds <btatnguyen@gmail.com>
2018-10-21 14:51:14 -0700

    spins added to main menu

---
+++ Assets/Scripts/BenScripts/ImageSpinner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageSpinner : MonoBehaviour {
+
+    public float rotationalSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+            transform.Rotate(0, 0, Time.deltaTime * rotationalSpeed, Space.World);
+    }
+}
