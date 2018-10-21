a5bbcd431a8089360ed856e3ff0fca4a6dbad3df
syy1125 <syy1125@hotmail.com>
2018-10-20 20:28:53 -0700

    Make player movement and collision respect QGB stats

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,32 +1,42 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MovementController : MonoBehaviour
 {
-	public float InputStrength;
+	[FormerlySerializedAs("InputStrength")]
+	public float BaseInputStrength;
+
 	public string HorizontalAxisName;
 	public string VerticalAxisName;
-	public float maxSpinsPerSecond;
+	public float MaxSpinsPerSecond;
+	public QuantumGyroBlade QGB;
+	public Sprite QGBSprite;
 
 	private Rigidbody2D _rigidbody2D;
-	private GameObject sprite;
+	private GameObject _sprite;
 
 	private void Start()
 	{
 		_rigidbody2D = GetComponent<Rigidbody2D>();
-		sprite = transform.GetChild(0).gameObject;
+		_sprite = transform.GetChild(0).gameObject;
+		_sprite.GetComponent<SpriteRenderer>().sprite = QGBSprite;
 	}
 
 	void Update()
 	{
-		sprite.transform.Rotate(0, 0, Mathf.Min(_rigidbody2D.velocity.magnitude * 0.5f, maxSpinsPerSecond) * Time.deltaTime * 360);
+		_sprite.transform.Rotate(
+			0,
+			0,
+			Mathf.Min(_rigidbody2D.velocity.magnitude * 0.5f, MaxSpinsPerSecond) * Time.deltaTime * 360
+		);
 	}
 
 	void FixedUpdate()
 	{
 		float horiz = Input.GetAxis(HorizontalAxisName);
 		float vert = Input.GetAxis(VerticalAxisName);
-		Vector2 movementForce = new Vector2(horiz, vert).normalized * InputStrength;
+		Vector2 movementForce = new Vector2(horiz, vert).normalized * BaseInputStrength * QGB.Acceleration;
 		float degrees = Vector2.Angle(_rigidbody2D.velocity, movementForce);
 		if (150 > degrees)
 		{
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,44 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class PlayerCollision : MonoBehaviour 
+public class PlayerCollision : MonoBehaviour
 {
-	public float collisionDamping;
+	[FormerlySerializedAs("collisionDamping")]
+	public float CollisionDamping;
+
+	public QuantumGyroBlade QGB;
 
 	private Rigidbody2D _rigidbody2D;
 
-	void Start () 
+	private void Start()
 	{
 		_rigidbody2D = GetComponent<Rigidbody2D>();
 	}
-	
-	void OnCollisionEnter2D(Collision2D col)
-    {
-		GameObject other = col.gameObject;
-		Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
-		if(rb != null && other.CompareTag("Player"))
-		{
-			// Debug.Log("mine : " + _rigidbody2D.velocity);
-			// Debug.Log("other: " + rb.velocity);
-			AudioManager.instance.PlayClangSound();
-			float mag = _rigidbody2D.velocity.magnitude * collisionDamping;
-			_rigidbody2D.velocity = (transform.position - other.transform.position) * rb.velocity.magnitude * collisionDamping;
-			rb.velocity = (other.transform.position - transform.position) * mag;
-		}
-	}
 
-	void OnTriggerEnter2D(Collider2D col)
+	private void OnCollisionEnter2D(Collision2D col)
 	{
 		GameObject other = col.gameObject;
 		Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
-		if(rb != null && other.CompareTag("Player"))
+		QuantumGyroBlade opponentQGB = other.GetComponent<MovementController>().QGB;
+
+		if (rb != null && other.CompareTag("Player"))
 		{
-			// Debug.Log("mine : " + _rigidbody2D.velocity);
-			// Debug.Log("other: " + rb.velocity);
-			float mag = _rigidbody2D.velocity.magnitude * collisionDamping;
-			_rigidbody2D.velocity = (transform.position - other.transform.position) * rb.velocity.magnitude * collisionDamping;
-			rb.velocity = (other.transform.position - transform.position) * mag;
+			AudioManager.instance.PlayClangSound();
+			float mag = _rigidbody2D.velocity.magnitude;
+			_rigidbody2D.velocity = (transform.position - other.transform.position)
+			                        * rb.velocity.magnitude
+			                        * CollisionDamping
+			                        * opponentQGB.Power
+			                        / QGB.Resistance;
+			rb.velocity = (other.transform.position - transform.position)
+			              * mag
+			              * CollisionDamping
+			              * QGB.Power
+			              / opponentQGB.Resistance;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		GameObject other = col.gameObject;
+		Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+		QuantumGyroBlade opponentQGB = other.GetComponent<MovementController>().QGB;
+
+		if (rb != null && other.CompareTag("Player"))
+		{
+			AudioManager.instance.PlayClangSound();
+			float mag = _rigidbody2D.velocity.magnitude;
+			_rigidbody2D.velocity = (transform.position - other.transform.position)
+			                        * rb.velocity.magnitude
+			                        * CollisionDamping
+			                        * opponentQGB.Power
+			                        / QGB.Resistance;
+			rb.velocity = (other.transform.position - transform.position)
+			              * mag
+			              * CollisionDamping
+			              * QGB.Power
+			              / opponentQGB.Resistance;
 		}
 	}
 }
