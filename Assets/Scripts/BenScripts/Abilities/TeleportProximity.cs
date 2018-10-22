2ea9c80495dc9b302f6c8959580bbe6520e8edb4
KupoGrounds <btatnguyen@gmail.com>
2018-10-22 04:55:14 -0700

    new proximity teleport ability
    teleport that is based on player position, rather than ability to teleport around the entire stage

---
+++ Assets/Scripts/BenScripts/Abilities/TeleportProximity.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportProximity : MonoBehaviour {
+
+    public float cooldown = 5.0f;
+    public float maxRadius = 5.0f;
+    public string abilityKey;
+    public bool ready;
+
+    private float stopwatch;
+
+    private void Start()
+    {
+        stopwatch = 0.0f;
+        ready = false;
+    }
+
+    private void Teleport()
+    {
+        Rigidbody2D rb = this.gameObject.GetComponent<Rigidbody2D>();
+
+        if (rb == null)
+        {
+            Debug.Log("TeleportProximity Error: Rigidbody2D does not exist. Teleport failed.");
+        }
+        else
+        {
+            Vector2 newPosition = new Vector2(rb.transform.position.x, rb.transform.position.y)
+                                    + Random.insideUnitCircle * maxRadius;
+
+            rb.transform.position = newPosition;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        stopwatch += Time.deltaTime;
+
+        if (stopwatch > cooldown)
+            ready = true;
+
+        if(Input.GetButtonDown(abilityKey) && ready == true)
+        {
+            Teleport();
+            ready = false;
+            stopwatch = 0.0f;
+        }
+	}
+}
