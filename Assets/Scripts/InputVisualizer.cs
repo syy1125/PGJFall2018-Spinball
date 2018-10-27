86aff1ca16c60290ecd581dcc171fe0d63c055a4
syy1125 <syy1125@hotmail.com>
2018-10-26 22:50:30 -0700

    Visualize input
    Set up scene to visualize input controls.

---
+++ Assets/Scripts/InputVisualizer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class InputVisualizer : MonoBehaviour
+{
+	public bool Raw;
+	public float Scale;
+	public string Horizontal;
+	public string Vertical;
+
+	private void Update()
+	{
+		transform.localPosition = Scale * (Raw
+			                     ? new Vector3(Input.GetAxisRaw(Horizontal), Input.GetAxisRaw(Vertical))
+			                     : new Vector3(Input.GetAxis(Horizontal), Input.GetAxis(Vertical)));
+	}
+}
