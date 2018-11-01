e769b02043ebd004d846957c3bd4a4eb29b333ec
Eric Kim <ericpaulkim@gmail.com>
2018-11-01 15:54:44 -0700

    Revert "Revert "Photon Testing""
    This reverts commit eb42a559d11dd7073fa9d6fd4c85ea36fd193a7d.

---
+++ Assets/Scripts/Network/NetworkPlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkPlayerManager : MonoBehaviourPunCallbacks
+{
+	public static GameObject localPlayer;
+
+	void Start () 
+	{
+		if(photonView.IsMine)
+		{
+			localPlayer = this.gameObject;
+			Camera.main.GetComponent<CameraController>().players.Add(this.gameObject);
+		}
+	}
+
+	// void OnDestroy()
+	// {
+	// 	Camera.main.GetComponent<CameraController>().players.Remove(this.gameObject);
+	// }
+
+}
